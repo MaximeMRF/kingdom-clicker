832f5609282c09cb59e0591b0432d3e88fda1674
MaximeMRF <57860498+MaximeMRF@users.noreply.github.com>
2020-10-14 10:21:44 -0400

    add β

--- simplify.js
+++ simplify.js
@@ -3,7 +3,17 @@ Par exemple 100 000 devient 1 α
 Cette fonction prend deux arguments le nombre à convertir (e)
 Et l'(id) qui définit l'id html dans lequel ce résultat va être rendu
 */
-const simplify = (e,id)=> Math.round(e) >= 100000 ? id.innerHTML = Math.round(e/100000)+' α': id.innerHTML = Math.round(e); 
+//const simplify = (e,id)=> Math.round(e) >= 100000 ? id.innerHTML = Math.round(e/100000)+' α': id.innerHTML = Math.round(e); 
+
+const simplify = (e, id) => {
+
+  if (Math.round(e) >= 100000 && Math.round(e) =< 100000000)
+    id.innerHTML = Math.round(e/100000)+' α';
+  else if (Math.round(e) >= 100000000)
+    id.innerHTML = Math.round(e/100000000)+' β';
+  else
+    id.innerHTML = Math.round(e);
+}
 
 
 
